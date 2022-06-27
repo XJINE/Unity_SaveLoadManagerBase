971659619d6acec7d9934932f55068b56879f2f1
XJINE <umeda@team-lab.com>
2022-06-27 15:16:10 +0900

    First commit.

---
+++ Assets/Packages/SaveLoadManagerBase/SaveLoadManagerBase.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public abstract class SaveLoadManagerBase : SingletonMonoBehaviour<SaveLoadManagerBase>
+{
+    #region Field
+
+    [SerializeField]
+    private string saveDir;
+
+    #endregion Field
+
+    #region Property
+
+    public string SaveDir => string.IsNullOrWhiteSpace(saveDir) ? SceneManager.GetActiveScene().name : saveDir;
+
+    #endregion Property
+
+    #region Method
+
+    public abstract void Save();
+    public abstract void Load();
+
+    #endregion Method
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# SaveLoadManagerBase
+# Unity_SaveLoadManagerBase
+
+Base class of to save and load data.
+
+## Importing
+
+You can use Package Manager or import it directly.
+
+```
+https://github.com/XJINE/Unity_SaveLoadManagerBase.git?path=Assets/Packages/SaveLoadManagerBase
+```
+
+NOTE: Because of the limitation of UnityPackageManager, it needs to install dependencies manually.
+
+### Dependencies
+
+This project use following resources.
+
+- https://github.com/XJINE/Unity_SingletonMonoBehaviour
