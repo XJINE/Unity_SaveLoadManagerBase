587b29a2b073bfa4b35158f072262fd07148f359
XJINE <xjine.andorelseif@gmail.com>
2025-01-20 19:02:29 +0900

    Update to use UnityEvent as Action.

--- Assets/Packages/SaveLoadManagerBase/SaveLoadManagerBase.cs
+++ Assets/Packages/SaveLoadManagerBase/SaveLoadManagerBase.cs
@@ -1,25 +1,52 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public abstract class SaveLoadManagerBase : SingletonMonoBehaviour<SaveLoadManagerBase>
 {
     #region Field
 
-    [SerializeField]
-    private string saveDir;
+    [SerializeField] protected string saveDir;
+
+    [SerializeField] protected UnityEvent onPreSave;
+    [SerializeField] protected UnityEvent onSave;
+    [SerializeField] protected UnityEvent onPostSave;
+
+    [SerializeField] protected UnityEvent onPreLoad;
+    [SerializeField] protected UnityEvent onLoad;
+    [SerializeField] protected UnityEvent onPostLoad;
 
     #endregion Field
 
     #region Property
 
     public virtual string SaveDir => string.IsNullOrWhiteSpace(saveDir) ? SceneManager.GetActiveScene().name : saveDir;
+    
+    public UnityEvent OnPreSave  => onPreSave;
+    public UnityEvent OnSave     => onSave;
+    public UnityEvent OnPostSave => onPostSave;
+
+    public UnityEvent OnPreLoad  => onPreLoad;
+    public UnityEvent OnLoad     => onLoad;
+    public UnityEvent OnPostLoad => onPostLoad;
 
     #endregion Property
 
     #region Method
 
-    public abstract void Save();
-    public abstract void Load();
+    public virtual void Save()
+    {
+        onPreSave .Invoke();
+        onSave    .Invoke();
+        onPostSave.Invoke();
+    }
+
+    public virtual void Load()
+    {
+        onPreLoad .Invoke();
+        onLoad    .Invoke();
+        onPostLoad.Invoke();
+    }
 
     #endregion Method
 }
